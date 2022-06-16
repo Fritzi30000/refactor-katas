799f349235855886543fa6ae1d28060b68e8d1b1
maja.chlodnicka <maja.chlodnicka@displate.com>
2022-06-16 19:17:46 +0200

    refactor(tell-dont-ask): remove unnecessary comments, add sections to OrderCreationUseCaseTest.php

--- tests/TellDontAsk/UseCase/OrderCreationUseCaseTest.php
+++ tests/TellDontAsk/UseCase/OrderCreationUseCaseTest.php
@@ -20,62 +20,45 @@ use RefactorKatas\Tests\TellDontAsk\Doubles\TestOrderRepository;
 class OrderCreationUseCaseTest extends TestCase
 {
     private TestOrderRepository $orderRepository;
-
-    private Category $food;
-
-    private InMemoryProductCatalog $productCatalog;
-
     private OrderCreationUseCase $useCase;
 
     public function setUp(): void
     {
         $this->orderRepository = new TestOrderRepository();
-        $this->food = new Category();
-        $this->food->setName('food');
-        $this->food->setTaxPercentage(10.0);
-
-        $product1 = new Product();
-        $product1->setName('salad');
-        $product1->setPrice(3.56);
-        $product1->setCategory($this->food);
-
-        $product2 = new Product();
-        $product2->setName('tomato');
-        $product2->setPrice(4.65);
-        $product2->setCategory($this->food);
-
-        $products = [$product1, $product2];
-
-        $this->productCatalog = new InMemoryProductCatalog(...$products);
-        $this->useCase = new OrderCreationUseCase($this->orderRepository, $this->productCatalog);
+        $food = new Category('food', 10.0);
+        $productCatalog = new InMemoryProductCatalog(
+            new Product('salad', 3.56, $food),
+            new Product('tomato', 4.65, $food)
+        );
+        $this->useCase = new OrderCreationUseCase($this->orderRepository, $productCatalog);
     }
 
-    /**
-     * @test
-     */
-    public function sellMultipleItems(): void
+    public function testShouldSellMultipleItems(): void
     {
-        $saladRequest = new SellItemRequest(2, "salad");
+        // Given
+        $request = new SellItemsRequest(
+            new SellItemRequest(2, "salad"),
+            new SellItemRequest(3, "tomato")
+        );
 
-        $tomatoRequest = new SellItemRequest(3, "tomato");
-
-        $request = new SellItemsRequest($saladRequest, $tomatoRequest);
-
+        // When
         $this->useCase->run($request);
 
+        // Then
         $insertedOrder = $this->orderRepository->getSavedOrder();
         $products = $insertedOrder->getItems();
         $this->assertEquals(OrderStatus::CREATED, $insertedOrder->getStatus()->getType());
-        $this->assertEquals($insertedOrder->getTotal(), 23.20);
-        $this->assertEquals($insertedOrder->getTax(), 2.13);
+        $this->assertEquals(23.20, $insertedOrder->getTotal());
+        $this->assertEquals(2.13, $insertedOrder->getTax());
         $this->assertEquals("EUR", $insertedOrder->getCurrency());
-        $this->assertEquals(2, count($products));
+        $this->assertCount(2, $products);
+
+        //TODO: this should be moved and checked somewhere else
         $this->assertEquals("salad", $products[0]->getProduct()->getName());
         $this->assertEquals(3.56, $products[0]->getProduct()->getPrice());
         $this->assertEquals(2, $products[0]->getQuantity());
         $this->assertEquals(7.84, $products[0]->getTaxedAmount());
         $this->assertEquals(0.72, $products[0]->getTax());
-
         $this->assertEquals("tomato", $products[1]->getProduct()->getName());
         $this->assertEquals(4.65, $products[1]->getProduct()->getPrice());
         $this->assertEquals(3, $products[1]->getQuantity());
@@ -83,14 +66,16 @@ class OrderCreationUseCaseTest extends TestCase
         $this->assertEquals(1.41, $products[1]->getTax());
     }
 
-    /**
-     * @test
-     */
-    public function unknownProduct(): void
+    public function testShouldNotSellUnknownProduct(): void
     {
+        // Expects
         $this->expectException(UnknownProductException::class);
+
+        // Given
         $unknownProductRequest = new SellItemRequest(1, 'unknown product');
         $request = new SellItemsRequest($unknownProductRequest);
+
+        // When
         $this->useCase->run($request);
     }
 
